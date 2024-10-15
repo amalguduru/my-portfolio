2264eb3f668837d47a51cc2e66b784cb0ac2eda2
amalguduru <amalguduru.com>
2024-10-15 13:25:04 -0400

    updated resume

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -37,7 +37,7 @@ const HeroSection = () => {
 
           <div>
             <Link
-              href="/files/Resume-2.pdf"
+              href="/files/Amal_Resume.pdf"
               target="_blank"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-red-500 to-red-500 hover:bg-slate-800 text-white mt-3"
             >
